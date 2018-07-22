b25842059706b00704f513c092de385c3a606d62
irwancahyt <sdirwancahyanto@gmail.com>
2018-07-22 10:25:42 +0700

    latihan

---
+++ exercises-14.js
@@ -0,0 +1,38 @@
+function sorting(arrNumber) {
+   arrSort = arrNumber.sort(function(value1,value2)     {return value2-value1})
+      return arrSort
+
+}
+function getTotal(arrNumber) {
+
+  var b =  sorting(arrNumber)
+  var tamp = 0
+  for(var i = 0 ; i < b.length ; i++){
+          if(arrSort[0] === arrSort[i]){
+            tamp += 1
+          }
+
+    }
+    if (tamp === 0){
+      return '""'
+    }else{
+
+    return 'angka paling besar adalah '+ arrSort[0] + ' dan jumlah kemunculan sebanyak '+ tamp +' kali'
+    }
+
+}
+  function mostFrequentLargestNumbers(arrNumber) {
+   var listSort = sorting(arrNumber);
+   var countHighest = getTotal(listSort);
+  return  countHighest;
+}
+
+ console.log(mostFrequentLargestNumbers([2, 8, 4, 6, 8, 5, 8, 4]));
+// 'angka paling besar adalah 8 dan jumlah kemunculan sebanyak 3 kali'
+ console.log(mostFrequentLargestNumbers([122, 122, 130, 100, 135, 100, 135, 150]));
+// 'angka paling besar adalah 150 dan jumlah kemunculan sebanyak 1 kali'
+
+console.log(mostFrequentLargestNumbers([1, 1, 1, 1]));
+// 'angka paling besar adalah 1 dan jumlah kemunculan sebanyak 4 kali'
+
+console.log(mostFrequentLargestNumbers([]))
